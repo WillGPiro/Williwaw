4b694bf3f50d9a48af987c3ede507a8038b3ffea
Will G Piro <willgpiro@gmail.com>
2020-07-06 12:32:33 -0700

    add Homepage

---
+++ src/components/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Home = () => {
+
+  return (
+    <>
+      <button>Oregon Surf Information</button>
+      <button>Washington Surf Information</button>
+    </>
+  );
+};
+
+export default Home;
+
