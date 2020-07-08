f15780ad654325be652c7f97713e63a88862345f
Will G Piro <willgpiro@gmail.com>
2020-07-07 18:06:21 -0700

    individual icon for la push, wa

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -12,6 +12,10 @@ class App extends Component {
       lat: 45.976098,
       lng: -123.946844,
     },
+    laPush: {
+      lat: 47.903230,
+      lng: -124.633203,
+    },
     zoom: 13,
   }
 
@@ -22,8 +26,16 @@ class App extends Component {
     popupAnchor:  [0, -41],
   });
 
+  laPushIcon = L.icon({
+    iconUrl:'surfboard-icon.png',
+    iconSize:     [25, 41],
+    iconAnchor:   [12.5, 41],
+    popupAnchor:  [0, -41],
+  })
+
   render(){
     const positionSeasideCove = [this.state.seasideCove.lat, this.state.seasideCove.lng];
+    const positionLaPush = [this.state.laPush.lat, this.state.laPush.lng];
 
     return (
       <Map className={styles.Map} center={positionSeasideCove} zoom={this.state.zoom}>
@@ -36,6 +48,12 @@ class App extends Component {
                 Seaside Cove
           </Popup>
         </Marker>
+        <Marker position={positionLaPush} icon={this.laPushIcon}>
+          <Popup>
+                La Push Cove
+          </Popup>
+        </Marker>
+
         <div>Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
       </Map>
     );
