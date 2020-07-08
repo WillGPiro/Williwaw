090780789accb13874770f06366b7fb3b62e871a
Will G Piro <willgpiro@gmail.com>
2020-07-07 17:01:57 -0700

    leaflet-react map running

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,12 +1,44 @@
-import React from 'react';
-import Routes from '../Router/Routes.jsx';
+import React, { Component } from 'react';
+// import Routes from '../Router/Routes.jsx';
+import L from 'leaflet';
+import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
+import styles from './App.css';
 
-export default function App() {
-  return (
-    <Routes/>
-  );
+const myIcon = L.icon({
+  iconUrl:'marker-icon.png',
+  iconSize: [25, 41],
+  iconAnchor: [12.5, 41],
+  popupAnchor: [0, -41],
+});
+
+
+class App extends Component {
+  
+  state = {
+    lat: 48.275030,
+    lng: -120.629552,
+    zoom: 10,
+  }
+
+  render(){
+    const position = [this.state.lat, this.state.lng];
+    return (
+      <Map className={styles.Map} center={position} zoom={this.state.zoom}>
+        <TileLayer
+          attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={position} icon={myIcon}>
+          <Popup>
+                A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </Map>
+    );
+  }
 }
 
+export default App;
 
 
 
---
+++ src/components/SimpleExample/simple.js
@@ -0,0 +1,90 @@
+import React, { Component } from 'react';
+import { Map, LayersControl, TileLayer, Marker, Popup } from 'react-leaflet';
+
+const { BaseLayer, Overlay } = LayersControl;
+
+export default class SimpleExample extends Component {
+  state = {
+    lat: 0,
+    lng: 0,
+    zoom: 1,
+
+    pickerOpen: true,
+    pickerLat: -23,
+    pickerLng: -42,
+
+    overlay: 'wind'
+  };
+
+  componentDidMount() {
+    let interval = setInterval(() => {
+      this.setState({
+        pickerLat: this.state.pickerLat + 1,
+        pickerLng: this.state.pickerLng + 1
+      });
+    }, 1000);
+
+    setTimeout(() => {
+      clearInterval(interval);
+      this.setState({ pickerOpen: false });
+    }, 6000);
+
+    setTimeout(() => {
+      this.setState({ pickerOpen: true, pickerLat: 25, pickerLng: 40 });
+    }, 7000);
+  }
+
+  render() {
+    const position = [this.state.lat, this.state.lng];
+
+    return (
+      <Map
+        className="leaflet-container"
+        windyKey={this.props.windyKey}
+        windyLabels={false}
+        windyControls={false}
+        overlay={this.state.overlay}
+        overlayOpacity={0.5}
+        particlesAnim={false}
+        zoom={this.state.zoom}
+        center={[this.state.lat, this.state.lng]}
+        removeWindyLayers
+        onWindyMapReady={() => {
+          console.log('Windy Map Loaded!');
+        }}
+        pickerPosition={
+          this.state.pickerOpen
+            ? [this.state.pickerLat, this.state.pickerLng]
+            : null
+        }
+        onPickerOpened={latLng => console.log('Picker Opened', latLng)}
+        onPickerMoved={latLng => {
+          console.log('Picker Moved', latLng);
+          this.setState({
+            pickerLat: latLng.lat,
+            pickerLng: latLng.lon
+          });
+        }}
+        onPickerClosed={() => console.log('Picker Closed')}
+        mapElements={
+          <React.Fragment>
+            <LayersControl>
+              <BaseLayer checked name="OSM">
+                <TileLayer
+                  attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                  url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+              </BaseLayer>
+            </LayersControl>
+
+            <Marker position={position}>
+              <Popup>
+                A pretty CSS3 popup. <br /> Easily customizable.
+              </Popup>
+            </Marker>
+          </React.Fragment>
+        }
+      />
+    );
+  }
+}
